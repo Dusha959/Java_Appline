baf04b62869936134f6d7d5fcb7ac6a502331f4b
Dusha959 <troa2001.95@mail.ru>
2020-11-25 15:42:32 +0300

    25.11

---
+++ Java для начинающих/задача №2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 14.5;
+        byte num2 = 127;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
